57cdb4ca90ea94448b2b7bccd79bf86f7062ff96
Marvkare <marvkare_games@yahoo.com>
2020-12-25 19:41:04 -0600

    gg

--- public/js/rememberThis.js
+++ public/js/rememberThis.js
@@ -1,31 +1,9 @@
-
-
-let wordOne = getEC('wordOne');
-let wordTwo = getEC('wordTwo');
-let agregarBtn = getEC('agregarBtn');
-let boxOne = getEC('boxOne');
-let boxTwo = getEC('boxTwo');
-let generadorHtml = getEC('generadorHtml');
-let generadorBox = getEC('generadorBox')
-let wordOneA = [];
-let wordTwoA = [];
-
-agregarBtn.onclick = () =>{
-
-    if(wordOne.value && wordTwo.value){
-        
-        wordOneA.push(wordOne.value);
-        wordTwoA.push(wordTwo.value)
-        //wordTwo.push(wordTwo.vahlue);       
-            addElement(wordOneA, wordTwoA)        
-        
-    }else{
-        console.log('No a escrito nunguna palabra para agregar')
-    }
-}
+let wordOne = getEC('wordOne')
+let wordTwo = getEC('wordTwo')
 
 function getEC(e) {
     return document.getElementById(e);
 }
 
-console.log('hola')
+wordOne.style.background = 'red';
+
